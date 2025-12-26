061331d5b78613eba39a8c276fbe60e8965c191d
tushar-cr7 <tusharrjoshi2007@gmail.com>
2025-12-26 20:18:37 +0530

    Add breadth first search traversal of graph

---
+++ Graphs/BFS.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+void bfs(int start, vector<vector<int>>& adj, int n) {
+    vector<bool> visited(n, false);
+    queue<int> q;
+
+    visited[start] = true;
+    q.push(start);
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        cout << node << " ";
+
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+}
+
+int main() {
+    int n = 5;
+    vector<vector<int>> adj(n);
+
+    adj[0] = {1, 2};
+    adj[1] = {0, 3};
+    adj[2] = {0, 4};
+    adj[3] = {1};
+    adj[4] = {2};
+
+    cout << "BFS Traversal: ";
+    bfs(0, adj, n);
+
+    return 0;
+}
