7202b20da2901aabb70d2789976084280d128a86
tushar-cr7 <tusharrjoshi2007@gmail.com>
2025-12-26 20:06:36 +0530

    Add selection sort implementation

---
+++ Sorting/SelectionSort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n) {
+    for(int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for(int j = i + 1; j < n; j++) {
+            if(arr[j] < arr[minIndex])
+                minIndex = j;
+        }
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = 5;
+
+    selectionSort(arr, n);
+
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
